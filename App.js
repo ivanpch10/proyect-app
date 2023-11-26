df2b069da18f14f86745be76b3d2706096938345
Ivan Perez Charmac <ivanpch@live.com.ar>
2023-11-26 20:07:06 -0300

    3ra Clase

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola, Coder!</Text>
-      <Text>Este es mi primer proyecto mobile</Text>
+      <View style={styles.inputContainer}>
+      <TextInput 
+      style={styles.input}
+      placeholder='Product'
+      />
+      <Button title="ADD"/>
+      </View>
+        <View style={styles.listContainer}>
+          <View style={styles.cardProduct}>
+            <Text style={styles.cardTitle}>Coca Cola</Text>
+            <Text>2500 $</Text>
+            <Button title= "DEL"></Button>
+            </View>
+            <View style={styles.listContainer}>
+          <View style={styles.cardProduct}>
+            <Text style={styles.cardTitle}>Fanta</Text>
+            <Text>2200 $</Text>
+            <Button title= "DEL"></Button>
+            </View>
+            <View style={styles.listContainer}>
+          <View style={styles.cardProduct}>
+            <Text style={styles.cardTitle}>Aquarius</Text>
+            <Text>1800 $</Text>
+            <Button title= "DEL"></Button>
+            </View>
+        </View>
+      </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'start',
+    marginTop: 22,
   },
+  inputContainer: {
+    flexDirection: "row",
+    alignSelf: "stretch",
+    justifyContent: "space-around"
+  },
+  input: {
+    borderWidth: 4,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    width:200,
+  },
+  listContainer:{
+    
+    width:"100%"
+  },
+  cardProduct: {
+    flexDirection:"row",
+    padding:10,
+    margin:10,
+    justifyContent:"space-around",
+    alignItems: "center",
+    borderWidth:4,
+  },
+  cardTitle:{
+
+  }
 });
