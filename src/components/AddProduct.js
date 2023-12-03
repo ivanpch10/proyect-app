6d86b93a33b764729fc5161dc51dbda8fb7311e2
Ivan Perez Charmac <ivanpch@live.com.ar>
2023-12-02 22:10:29 -0300

    2da entrega

--- src/components/AddProduct.js
+++ src/components/AddProduct.js
@@ -1,45 +1,65 @@
 import {View, TextInput, Button, StyleSheet} from "react-native"
 
 const AddProduct = ({valueTitle, valuePrice, valueQuantity, onChangeTitle, onChangePrice, onChangeQuantity, addProduct}) => {
-   return (
-   <View style={styles.inputContainer}>
-      <TextInput 
-      style={styles.input}
-      placeholder="Nombre"
-      value={valueTitle}
-      onChangeText={(t)=> onChangeTitle(t)}
-      />
-      <TextInput 
-      style={styles.input}
-      placeholder='Precio'
-      value={valuePrice}
-      onChangeText={(t)=> onChangePrice(t)}
-      />
-      <TextInput 
-      style={styles.input}
-      placeholder='Cantidad'
-      value={valueQuantity}
-      onChangeText={(t)=> onChangeQuantity(t)}
-      />
-      <Button title="ADD" onPress={addProduct}
-      //onPress={()=> console.log(`El Producto agregado es: ${newTitleProduct} y su Precio es: ${newPriceProduct}`)}
-      />
-    </View>
-); };
-const styles = StyleSheet.create({
-    inputContainer: {
-        flexDirection: "column",
-        alignItems: "center",
-        width:"100%",
-        justifyContent: "space-around"
-      },
-      input: {
-        borderWidth: 4,
-        paddingHorizontal: 10,
-        paddingVertical: 5,
-        width:200,
-        margin:10,
-      }
-})
+  return (
+    <View style={styles.inputContainer}>
+       <TextInput 
+       style={styles.input}
+       placeholder="Nombre"
+       value={valueTitle}
+       onChangeText={(t)=> onChangeTitle(t)}
+       />
+       <TextInput 
+       style={styles.input}
+       placeholder='Precio'
+       value={valuePrice}
+       keyboardType="numeric"
+       onChangeText={(t)=> onChangePrice(t)}
+       />
+       <TextInput 
+       style={styles.input}
+       placeholder='Cantidad'
+       keyboardType="numeric"
+       value={valueQuantity}
+       onChangeText={(t)=> onChangeQuantity(t)}
+       />
+       <Button 
+       title="ADD" 
+       onPress={addProduct}
+       style={styles.addButton}
+       titleStyle={styles.addButtonText}
+       //onPress={()=> console.log(`El Producto agregado es: ${newTitleProduct} y su Precio es: ${newPriceProduct}`)}
+       />
+     </View>
+ ); };
+ const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    marginBottom: 20,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  addButton: {
+    backgroundColor: 'blue',
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  addButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+ 
+ })
 
 export default AddProduct
--- src/components/ModalDelete.js
+++ src/components/ModalDelete.js
@@ -2,34 +2,48 @@ import {Modal, View, Text, Button,StyleSheet} from "react-native"
 
 const ModalDelete = ({product,visible, onModal, onDelete}) => {
 
-    return (     <Modal 
-                    visible={visible}
-                >
-                    <View style={styles.modalContainer}>   
-                        <View style={styles.modalContent}>
-                            <Text style={styles.modalText}>Está seguro que desea eliminarlo?</Text>
-                            <Text style={styles.modalText}>{product.title}</Text>
-                            <Button title="Confirmar" onPress={onDelete}/>
-                            <Button title="Cerrar" onPress={()=>onModal(false)}/>
-                        </View>
-                    </View>
-                </Modal> 
+   
+  return (     <Modal 
+    visible={visible}
+>
+    <View style={styles.modalContainer}>   
+        <View style={styles.modalContent}>
+            <Text style={styles.modalText}>Está seguro que desea eliminarlo?</Text>
+            <Text style={styles.modalText}>{product.title}</Text>
+        <View style={styles.modalButtons}>
+            <Button title="Confirmar" onPress={onDelete}/>
+            <Button title="Cerrar" onPress={()=>onModal(false)}/>
+        </View>
+        </View>
+    </View>
+</Modal> 
 );};
 
 const styles = StyleSheet.create({
-    modalContainer:{
-        flex:1,
-        alignItems: "center",
-        justifyContent: "center"
-      },
-      modalContent:{
-        width: "80%",
-        borderWidth:2,
-        padding:10,
-        gap:10,
-      },
-      modalText:{
-        textAlign: "center"
-      }
+modalContainer: {
+flex: 1,
+justifyContent: 'center',
+alignItems: 'center',
+backgroundColor: 'rgba(0, 0, 0, 0.5)',
+},
+modalContent: {
+width: "80%",
+borderWidth: 2,
+padding: 10,
+gap: 10,
+borderColor: 'blue', // Por ejemplo, un nuevo color para el borde
+borderRadius: 8, // Agregando bordes redondeados
+backgroundColor: 'white', // Cambiando el color de fondo del contenido modal
+},
+modalText: {
+marginBottom: 20,
+fontSize: 18,
+},
+modalButtons: {
+flexDirection: 'row',
+justifyContent: 'space-around',
+width: '100%',
+marginTop: 10,
+},
 })
 export default ModalDelete
